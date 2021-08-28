c26f7a6a88c47f2b6a525ec6d971168faf21afa5
Makkonen Haile <nmhaile@usc.edu>
2021-08-27 17:57:33 -0700

    Add files via upload

--- backgroundscroll.js
+++ backgroundscroll.js
@@ -12,62 +12,6 @@ menu.addEventListener('click', mobileMenu);
 
 const faders = document.querySelectorAll(".fade-in");
 
-function disableScroll() {
-    document.body.style.overflow = "hidden";
-    document.querySelector("html").scrollTop = window.scrollY;
-  }
-  
-  function enableScroll() {
-    document.body.style.overflow = null;
-  }
-  if ($(window).width() > 700) {
-    disableScroll();
-    $(function () {
-      var isScrolling = false;
-      $(window).on("wheel", function (e) {
-        if (!isScrolling) {
-          isScrolling = true;
-  
-          var howFarFromTop = $(document).scrollTop(); //how far from the top have we scrolled?
-          var currentWindowHeight = $(window).height(); //current window height for responsiveness
-          var delta = e.originalEvent.deltaY; // just to know if it is scroll wheel up or down
-          //find out what is our offset from the top so we can now how far do we have to scroll to  the next / previous element
-          var currentSlide = Math.floor(howFarFromTop / currentWindowHeight); //approximate which slide is on screen at the moment
-  
-          if (delta > 0) {
-            //scroll down
-            smoothScroll(currentWindowHeight * (currentSlide + 1));
-          } else {
-            //scroll up
-            smoothScroll(currentWindowHeight * (currentSlide - 1));
-          }
-          setTimeout(function () {
-            isScrolling = false;
-          }, 1500);
-        }
-  
-        return false; // don't let the browser do the default scroll
-      });
-    });
-  
-    function smoothScroll(offsetPixels) {
-      if (offsetPixels < 0) {
-        //avoid negative numbers on the scroll up
-        offsetPixels = 0;
-      }
-  
-      //this function is just to make an animated scrolling transition
-      $("html, body").animate(
-        {
-          scrollTop: offsetPixels,
-        },
-        1500
-      );
-    }
-  } else {
-    enableScroll();
-  }
-
 
   const appearOptions = {
     threshold: 0,
